3c83e706aa8bf0d60a6026a5ab41d7615e2ccfee
Kolton Koester <koltonkoester10@gmail.com>
2022-09-27 15:15:11 -0400

    made a model folder which inculders a character model class based on the class of choice assignment which also inculdes the characters properties, constructor, and commented out methods. also worked out the character context and now building out some default characters

---
+++ DnDCharacter/DnDCharacter/CharacterContext.cs
@@ -0,0 +1,27 @@
+﻿using DnDCharacter.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DnDCharacter
+{
+    public class CharacterContext : DbContext
+    {
+        public DbSet<Character> Characters { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //string connection
+            string connectionString = "server=(localdb)\\mssqllocaldb;Database=CharacterOfChoiceAPI;Trusted_Connection=True;";
+            optionsBuilder.UseSqlServer(connectionString);
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder model)
+        {
+            model.Entity<Character>().HasData(
+                new Character() {Id = 1, },
+                new Character() { },
+                new Character() { }
+                );
+        }
+    }
+}
---
+++ DnDCharacter/DnDCharacter/Models/Character.cs
@@ -0,0 +1,116 @@
+﻿namespace DnDCharacter.Models
+{
+    public class Character
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int ArmorClass { get; set; }
+        public Dictionary<string, int> Inventory = new Dictionary<string, int>();
+        public int HitPoints { get; set; }
+        public int Speed { get; set; }
+        public int Experiance { get; set; }
+
+        public Character()
+        {
+            
+        }
+
+       /* public void CreateCharacter()
+        {
+            Console.Clear();
+            Console.WriteLine("Enter the name of your character");
+            Name = Console.ReadLine();
+            Console.WriteLine("Enter the Armor Class of your character");
+            ArmorClass = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the Hit Points of your character");
+            HitPoints = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the Speed of your character");
+            Speed = int.Parse(Console.ReadLine());
+            Console.Clear();
+        }
+        public void AddItemToInventory(string item, int amount)
+        {
+            // Add items to inventory
+            Inventory.Add(item, amount);
+            Console.Clear();
+            Console.WriteLine("YOUR INVENTORY");
+            foreach (var items in Inventory)
+            {
+                Console.WriteLine($"{items.Key} : {items.Value}");
+            }
+        }
+        public void RemoveItemFromInventory(string item, int amount)
+        {
+            //Removes amount of item from a list
+            if ((Inventory[item] -= amount) < 0)
+            {
+                Inventory.Remove(item);
+            }
+            Console.Clear();
+            Console.WriteLine("YOUR INVENTORY");
+            foreach (var items in Inventory)
+            {
+                Console.WriteLine($"{items.Key} : {items.Value}");
+            }
+        }
+        public void DecreaseArmorClass(int userInput)
+        {
+            // decrease Armor class by user input
+            Console.Clear();
+            ArmorClass -= userInput;
+        }
+        public void IncreaseArmorClass(int userInput)
+        {
+            // increase Armor class by user input
+            Console.Clear();
+            ArmorClass += userInput;
+        }
+        public void Attacked()
+        {
+            Random random = new Random();
+            if (ArmorClass > random.Next(40))
+            {
+                Console.Clear();
+                Console.WriteLine("YOU MISSED YOUR ATTACK!");
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("YOU HIT THE KILLER ROBOT!");
+                HitPoints -= 5;
+            }
+        }
+        public void Block()
+        {
+            Random random = new Random();
+            if (ArmorClass > random.Next(40))
+            {
+                Console.Clear();
+                Console.WriteLine("YOU BLOCKED THE KILLER ROBOTS ATTACK!");
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("THE KILLER ROBOT HIT YOU!");
+                Thread.Sleep(1000);
+                HitPoints -= 5;
+            }
+        }
+        public void Run()
+        {
+            Random random = new Random();
+            if (Speed > random.Next(40))
+            {
+                Console.Clear();
+                Console.WriteLine("YOU GOT AWAY!");
+                Thread.Sleep(3000);
+            }
+            else
+            {
+
+                Console.Clear();
+                Console.WriteLine("YOU CANT GET AWAY, YOU'RE NOT FAST ENOUGH!");
+            }
+        }*/
+    }
+}
