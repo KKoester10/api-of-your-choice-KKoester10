483eb80a96719c97369c99d0763c05af0ea63f19
Kolton Koester <koltonkoester10@gmail.com>
2022-09-28 17:49:53 -0400

    added in seed data for the character

--- DnDCharacter/DnDCharacter/CharacterContext.cs
+++ DnDCharacter/DnDCharacter/CharacterContext.cs
@@ -21,26 +21,26 @@ namespace DnDCharacter
         protected override void OnModelCreating(ModelBuilder model)
         {
             model.Entity<Party>().HasData(
-                new Party() { },
-                new Party() { }
+                new Party() {Id = 1, Name = "There can only be one"},
+                new Party() {Id = 2, Name = "well there can be another"}
                 );
 
             model.Entity<Character>().HasData(
-                new Character() { Id = 1, Name = "Bob", HitPoints = 30, Speed = 30, ArmorClass = 30, Experiance = 0 },
-                new Character() { Id = 2, Name = "Jedidia", HitPoints = 30, Speed = 30, ArmorClass = 30, Experiance = 250},
-                new Character() { Id = 3, Name = "Keb", HitPoints = 30, Speed = 30, ArmorClass = 30, Experiance = 150}
+                new Character() { Id = 1, PartyId = 1,PlayerName = "John",Name = "Bob" ,Class = "Fighter",Level = 0,Race = "Orc",Allignment = "Neutral Good",Background = "Entertainer",ProficiencyBonus = 2,Initiative = 3, HitPoints = 30, Speed = 30, ArmorClass = 30, Experiance = 0 },
+                new Character() { Id = 2, PartyId = 1,PlayerName = "Chris",Name = "Jedidia",Class = "Paladin",Level = 2,Race = "Human",Allignment = "Lawful Good",Background = "Far Traveler",ProficiencyBonus = -1,Initiative = 2 ,  HitPoints = 30, Speed = 30, ArmorClass = 30, Experiance = 250},
+                new Character() { Id = 3, PartyId = 2,PlayerName = "Mat",Name = "Keb",Class = "Warlock",Level = 5,Race = "Tiefling",Allignment = "Chaotic Evil",Background = "Soldier",ProficiencyBonus = 5,Initiative = 1,  HitPoints = 30, Speed = 30, ArmorClass = 30, Experiance = 150}
                 );
 
             model.Entity<Abilities>().HasData(
-                new Abilities() { },
-                new Abilities() { },
-                new Abilities() { }
+                new Abilities() { Id = 1, CharacterId = 1, Strength = 0, Dexterity = 0, Charisma = 0, Constitution = 0, Wisdom = 0, Intelligence = 0 },
+                new Abilities() { Id = 2, CharacterId = 2, Strength = 0, Dexterity = 0, Charisma = 0, Constitution = 0, Wisdom = 0, Intelligence = 0 },
+                new Abilities() { Id = 3, CharacterId = 3, Strength = 0, Dexterity = 0, Charisma = 0, Constitution = 0, Wisdom = 0, Intelligence = 0 }
                 );
 
             model.Entity<CharacterInventory>().HasData(
-                new CharacterInventory() { Id = 1, ItemName = "Gold", Amount = 150, CharacterId = 1 },
-                new CharacterInventory() { Id = 2, ItemName = "Gold", Amount = 250, CharacterId = 2 },
-                new CharacterInventory() { Id = 3, ItemName = "Gold", Amount = 350, CharacterId = 3 }
+                new CharacterInventory() { Id = 1,CharacterId = 1 , ItemName = "Gold", Amount = 150 },
+                new CharacterInventory() { Id = 2,CharacterId = 2 , ItemName = "Gold", Amount = 250 },
+                new CharacterInventory() { Id = 3,CharacterId = 3 , ItemName = "Gold", Amount = 350 }
                 );
         }
     }
